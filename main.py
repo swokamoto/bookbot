564117f176e03111c39c9512cf5331a9b0f8d2d2
swokamoto <swokamoto@gmail.com>
2025-08-25 00:07:34 -0400

    replaced hard links with sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from stats import get_word_count, get_char_count, sort_dict
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:   
@@ -12,4 +17,4 @@ def get_book_text(path_to_file):
 # get_book_text("books/frankenstein.txt")
 # get_word_count("books/frankenstein.txt")
 # get_char_count("books/frankenstein.txt")
-sort_dict("books/frankenstein.txt")
+sort_dict(sys.argv[1])
